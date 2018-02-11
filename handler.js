b8cff9c67f35a39e7c55493799bb9dd29bc4753f
roblesisaac <irobles1030@gmail.com>
2018-02-10 21:18:25 -0800

    Update handler.js

--- handler.js
+++ handler.js
@@ -43,123 +43,75 @@ module.exports.create = (event, context, callback) => {
 };
 
 module.exports.getOne = (event, context, callback) => {
-  return callback(null, {
-    statusCode: 200,
-    headers: {
-      /* Required for CORS support to work */
-      "Access-Control-Allow-Origin": "*",
-      /* Required for cookies, authorization headers with HTTPS */
-      "Access-Control-Allow-Credentials": true
-    },
-    body: JSON.stringify({
-      message: 'Hi ⊂◉‿◉つ from Public API',
-    }),
-  })
-  // context.callbackWaitsForEmptyEventLoop = false;
+  context.callbackWaitsForEmptyEventLoop = false;
 
-  // connectToDatabase()
-  //   .then(() => {
-  //     Note.findById(event.pathParameters.id)
-  //       .then(note => callback(null, {
-  //         statusCode: 200,
-  //         body: JSON.stringify(note)
-  //       }))
-  //       .catch(err => callback(null, {
-  //         statusCode: err.statusCode || 500,
-  //         headers: { 'Content-Type': 'text/plain' },
-  //         body: 'Could not fetch the note.'
-  //       }));
-  //   });
+  connectToDatabase()
+    .then(() => {
+      Note.findById(event.pathParameters.id)
+        .then(note => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(note)
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Could not fetch the note.'
+        }));
+    });
 };
 
 module.exports.getAll = (event, context, callback) => {
-  return callback(null, {
-    statusCode: 200,
-    headers: {
-      /* Required for CORS support to work */
-      "Access-Control-Allow-Origin": "*",
-      /* Required for cookies, authorization headers with HTTPS */
-      "Access-Control-Allow-Credentials": true
-    },
-    body: JSON.stringify({
-      message: 'Hi ⊂◉‿◉つ from Public API',
-    }),
-  })
-  // context.callbackWaitsForEmptyEventLoop = false;
+  context.callbackWaitsForEmptyEventLoop = false;
 
-  // connectToDatabase()
-  //   .then(() => {
-  //     Note.find()
-  //       .then(notes => callback(null, {
-  //         statusCode: 200,
-  //         body: JSON.stringify(notes)
-  //       }))
-  //       .catch(err => callback(null, {
-  //         statusCode: err.statusCode || 500,
-  //         headers: { 'Content-Type': 'text/plain' },
-  //         body: 'Could not fetch the notes.'
-  //       }))
-  //   });
+  connectToDatabase()
+    .then(() => {
+      Note.find()
+        .then(notes => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(notes)
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Could not fetch the notes.'
+        }))
+    });
 };
 
 module.exports.update = (event, context, callback) => {
-  return callback(null, {
-    statusCode: 200,
-    headers: {
-      /* Required for CORS support to work */
-      "Access-Control-Allow-Origin": "*",
-      /* Required for cookies, authorization headers with HTTPS */
-      "Access-Control-Allow-Credentials": true
-    },
-    body: JSON.stringify({
-      message: 'Hi ⊂◉‿◉つ from Public API',
-    }),
-  })
-  // context.callbackWaitsForEmptyEventLoop = false;
+  context.callbackWaitsForEmptyEventLoop = false;
 
-  // connectToDatabase()
-  //   .then(() => {
-  //     Note.findByIdAndUpdate(event.pathParameters.id, JSON.parse(event.body), { new: true })
-  //       .then(note => callback(null, {
-  //         statusCode: 200,
-  //         body: JSON.stringify(note)
-  //       }))
-  //       .catch(err => callback(null, {
-  //         statusCode: err.statusCode || 500,
-  //         headers: { 'Content-Type': 'text/plain' },
-  //         body: 'Could not fetch the notes.'
-  //       }));
-  //   });
+  connectToDatabase()
+    .then(() => {
+      Note.findByIdAndUpdate(event.pathParameters.id, JSON.parse(event.body), { new: true })
+        .then(note => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(note)
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Could not fetch the notes.'
+        }));
+    });
 };
 
 module.exports.delete = (event, context, callback) => {
-  return callback(null, {
-    statusCode: 200,
-    headers: {
-      /* Required for CORS support to work */
-      "Access-Control-Allow-Origin": "*",
-      /* Required for cookies, authorization headers with HTTPS */
-      "Access-Control-Allow-Credentials": true
-    },
-    body: JSON.stringify({
-      message: 'Hi ⊂◉‿◉つ from Public API',
-    }),
-  })
-  // context.callbackWaitsForEmptyEventLoop = false;
+  context.callbackWaitsForEmptyEventLoop = false;
 
-  // connectToDatabase()
-  //   .then(() => {
-  //     Note.findByIdAndRemove(event.pathParameters.id)
-  //       .then(note => callback(null, {
-  //         statusCode: 200,
-  //         body: JSON.stringify({ message: 'Removed note with id: ' + note._id, note: note })
-  //       }))
-  //       .catch(err => callback(null, {
-  //         statusCode: err.statusCode || 500,
-  //         headers: { 'Content-Type': 'text/plain' },
-  //         body: 'Could not fetch the notes.'
-  //       }));
-  //   });
+  connectToDatabase()
+    .then(() => {
+      Note.findByIdAndRemove(event.pathParameters.id)
+        .then(note => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify({ message: 'Removed note with id: ' + note._id, note: note })
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Could not fetch the notes.'
+        }));
+    });
 };
 
 // Reusable Authorizer function, set on `authorizer` field in serverless.yml
