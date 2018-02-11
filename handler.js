565c638470af23e46e865201cbf0554b2cdd4f35
roblesisaac <irobles1030@gmail.com>
2018-02-10 19:50:03 -0800

    Update handler.js

--- handler.js
+++ handler.js
@@ -65,16 +65,20 @@ module.exports.getAll = (event, context, callback) => {
 
   connectToDatabase()
     .then(() => {
-      Note.find()
-        .then(notes => callback(null, {
-          statusCode: 200,
-          body: JSON.stringify(notes)
-        }))
-        .catch(err => callback(null, {
-          statusCode: err.statusCode || 500,
-          headers: { 'Content-Type': 'text/plain' },
-          body: 'Could not fetch the notes.'
-        }))
+      callback(null, {
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Could not fetch the notes.'
+      });
+      // Note.find()
+      //   .then(notes => callback(null, {
+      //     statusCode: 200,
+      //     body: JSON.stringify(notes)
+      //   }))
+      //   .catch(err => callback(null, {
+      //     statusCode: err.statusCode || 500,
+      //     headers: { 'Content-Type': 'text/plain' },
+      //     body: 'Could not fetch the notes.'
+      //   }))
     });
 };
 
