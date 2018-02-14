1af235846de7fb8461867b4e712b890afb487d9c
roblesisaac <irobles1030@gmail.com>
2018-02-14 15:51:13 -0800

    Create api.js

---
+++ api.js
@@ -0,0 +1,17 @@
+module.exports.get = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase()
+    .then(() => {
+      Note.create(JSON.parse(event.body))
+        .then(note => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(note)
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Could not create the note.'
+        }));
+    });
+};
