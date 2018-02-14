9a7c682292e6216736e8c951d1a1bf42b4fac53f
roblesisaac <irobles1030@gmail.com>
2018-02-14 15:50:04 -0800

    Update handler.js

--- handler.js
+++ handler.js
@@ -3,27 +3,6 @@ const connectToDatabase = require('./db');
 const Note = require('./models/Notes');
 const sheet = require('./models/sheets');
 
-module.exports.sheet = (event, context, callback) => {
-  context.callbackWaitsForEmptyEventLoop = false;
-  connectToDatabase()
-    .then(() => {
-      let query = {};
-      if(event.queryStringParameters) {
-        query = event.queryStringParameters;
-      }
-      sheet.find(query)
-        .then(sheets => callback(null, {
-          statusCode: 200,
-          body: JSON.stringify(sheets)
-        }))
-        .catch(err => callback(null, {
-          statusCode: err.statusCode || 500,
-          headers: { 'Content-Type': 'text/plain' },
-          body: 'Could not fetch the sheets.'
-        }))
-    });
-};
-
 module.exports.create = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
 
