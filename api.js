4bab5e9f4b65daf31ca89ef6168c8d42a405bbd6
roblesisaac <irobles1030@gmail.com>
2018-02-14 16:33:50 -0800

    Update api.js

--- api.js
+++ api.js
@@ -12,27 +12,12 @@ module.exports.rest = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
   const siteUrl = event.pathParameters.sitename;
   const sheetName = event.pathParameters.sheet;
-  let model = prtcl[sheetName];
   connectToDatabase()
     .then(() => {
       callback(null, {
         statusCode: 200,
-        model: model,
         event: event,
         context: context
       });
     });
-  // connectToDatabase()
-  //   .then(() => {
-  //     Note.create(JSON.parse(event.body))
-  //       .then(note => callback(null, {
-  //         statusCode: 200,
-  //         body: JSON.stringify(note)
-  //       }))
-  //       .catch(err => callback(null, {
-  //         statusCode: err.statusCode || 500,
-  //         headers: { 'Content-Type': 'text/plain' },
-  //         body: 'Could not create the note.'
-  //       }));
-  //   });
 };
