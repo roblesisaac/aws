c98ead31bf6ca5d6443ca265cf14518738c8c2bd
roblesisaac <irobles1030@gmail.com>
2018-02-14 16:47:52 -0800

    Update api.js

--- api.js
+++ api.js
@@ -1,18 +1,8 @@
-const connectToDatabase = require('./db');
-const user = require('./models/users');
-const site = require('./models/sites');
-const sheet = require('./models/sheets');
-const prtcl = {
-  user: 'user protocol',
-  site: 'site  protocol',
-  sheet: 'sheet  protocol'
-};
-
 module.exports.rest = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Welcome to the api of ' + event.pathParameters.sitename + '.com !!',
+      message: 'Welcome to the api of .com !!',
       context: context,
       event: event
     }),
