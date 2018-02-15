fdc8e8695c7045cd2d7961510619c7eb606612db
roblesisaac <irobles1030@gmail.com>
2018-02-14 17:30:02 -0800

    Update api.js

--- api.js
+++ api.js
@@ -9,14 +9,19 @@ const prtcl = {
 };
 
 module.exports.rest = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Welcome to the api of .com !!',
-      context: context,
-      event: event
-    }),
-  };
+  context.callbackWaitsForEmptyEventLoop = false;
 
-  callback(null, response);
+  connectToDatabase()
+    .then(() => {
+        const response = {
+          statusCode: 200,
+          body: JSON.stringify({
+            message: 'Welcome to the api of .com !!',
+            context: context,
+            event: event
+          }),
+        };
+      
+        callback(null, response);
+    });
 }
