8aaf7ef774e2ec27a59628eea79af64e25be7f38
roblesisaac <irobles1030@gmail.com>
2018-02-14 17:19:17 -0800

    Update api.js

--- api.js
+++ api.js
@@ -10,15 +10,14 @@ const prtcl = {
 
 module.exports.rest = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
-  const siteUrl = event.pathParameters.siteUrl;
-  const sheetName = event.pathParamenter.sheet;
-  const method = prtcl[sheetName];
+  // const siteUrl = event.pathParameters.sitename;
+  // const sheetName = event.pathParamenter.sheet;
+  // const method = prtcl[sheetName];
   connectToDatabase()
     .then(() => {
       callback(null, {
         statusCode: 200,
         body: JSON.stringify({
-          method: method,
           event: event
         })
       })
