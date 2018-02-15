36c6d105fa5184a76fc0f1069303c22838f29cfd
roblesisaac <irobles1030@gmail.com>
2018-02-14 18:40:43 -0800

    Update api.js

--- api.js
+++ api.js
@@ -8,21 +8,29 @@ const prtcl = {
   sheets: 'sheet  protocol'
 };
 
+const post = function(model, event, callback) {
+  model.create(JSON.parse(event.body))
+    .then(data => callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    }))
+    .catch(err => callback(null, {
+      statusCode: err.statusCode || 500,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Could not create the note.'
+    }));
+};
+
 module.exports.rest = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
   const siteName = event.pathParameters.sitename;
   const sheetName = event.pathParameters.sheet;
-  const schema = prtcl[sheetName];
+  const model = prtcl[sheetName];
   const method = event.httpMethod;
   connectToDatabase()
     .then(() => {
-      if(method) {
-        callback(null, {
-          statusCode: 200,
-          body: JSON.stringify({
-            message: schema
-          })
-        });
+      if(model) {
+        post(model, event, callback);
       } else {
         callback(null, {
           statusCode: 200,
