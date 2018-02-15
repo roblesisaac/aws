2dc3cc07384d27ae0931292fe47efbc4be03ee50
roblesisaac <irobles1030@gmail.com>
2018-02-14 18:41:28 -0800

    Update handler.js

--- handler.js
+++ handler.js
@@ -194,12 +194,12 @@ module.exports.landingPage = (event, context, callback) => {
         el: "#app",
         methods: {
           create: function() {
-            axios.post("http://www.blockometry.com/plysheet/users", this.user).then(function(res){
+            axios.post("https://www.blockometry.com/plysheet/api/users", this.user).then(function(res){
               console.log(res.data)
             });
           },
           login: function() {
-            axios.post("http://www.blockometry.com/plysheet/auth", this.user).then(function(res){
+            axios.post("https://www.blockometry.com/plysheet/api/auth", this.user).then(function(res){
               console.log(res.data)
             });
           }
