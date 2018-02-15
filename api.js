fbd88791e99e7db1b6922a94457fd08f3cccfd98
roblesisaac <irobles1030@gmail.com>
2018-02-14 16:56:50 -0800

    Update api.js

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 const connectToDatabase = require('./db');
 const user = require('./models/users');
-const site = require('./models/sites');
+const sheet = require('./models/sheets');
 const prtcl = {
   user: 'user protocol',
   site: 'site  protocol',
