6c057c79aca81527e0e16099d7820a9ae0a5afe5
roblesisaac <irobles1030@gmail.com>
2018-02-14 16:19:15 -0800

    Update api.js

--- api.js
+++ api.js
@@ -1,17 +1,37 @@
-module.exports.get = (event, context, callback) => {
-  context.callbackWaitsForEmptyEventLoop = false;
+const user = require('./models/users');
+const site = require('./models/sites');
+const sheet = require('./models/sheets');
+const prtcl = {
+  user: 'user protocol',
+  site: 'site  protocol',
+  sheet: 'sheet  protocol'
+};
 
+module.exports.rest = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  const siteUrl = event.pathParameters.sitename;
+  const sheetName = event.pathParameters.sheet;
+  let model = prtcl[sheetName];
   connectToDatabase()
     .then(() => {
-      Note.create(JSON.parse(event.body))
-        .then(note => callback(null, {
-          statusCode: 200,
-          body: JSON.stringify(note)
-        }))
-        .catch(err => callback(null, {
-          statusCode: err.statusCode || 500,
-          headers: { 'Content-Type': 'text/plain' },
-          body: 'Could not create the note.'
-        }));
+      callback(null, {
+        statusCode: 200,
+        model: model,
+        event: event,
+        context: context
+      });
     });
+  // connectToDatabase()
+  //   .then(() => {
+  //     Note.create(JSON.parse(event.body))
+  //       .then(note => callback(null, {
+  //         statusCode: 200,
+  //         body: JSON.stringify(note)
+  //       }))
+  //       .catch(err => callback(null, {
+  //         statusCode: err.statusCode || 500,
+  //         headers: { 'Content-Type': 'text/plain' },
+  //         body: 'Could not create the note.'
+  //       }));
+  //   });
 };
