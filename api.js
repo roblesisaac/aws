22eacc76cbfcfe3021411ed560abc45e1e919317
roblesisaac <irobles1030@gmail.com>
2018-02-15 17:00:34 -0800

    Update api.js

--- api.js
+++ api.js
@@ -8,13 +8,13 @@ const models = {
 const res = function(model, event, callback) {
   const methods = { POST: "create", GET: "find", PUT: "findByIdAndUpdate", DELETE: "findByIdAndRemove" };
   const http = event.httpMethod;
-  let req = {};
+  let params = {};
   if(http === "POST") {
-    req = JSON.parse(event.body);
+    params = JSON.parse(event.body);
   } else if (http === 'GET') {
-    req = event.queryStringParameters;
+    params = event.queryStringParameters;
   }
-  model[methods[http]](req)
+  model[methods[http]](params)
     .then(data => callback(null, {
       statusCode: 200,
       body: JSON.stringify(data)
@@ -45,3 +45,57 @@ module.exports.rest = (event, context, callback) => {
       }
     });
 };
+
+// module.exports.getOne = (event, context, callback) => {
+//   context.callbackWaitsForEmptyEventLoop = false;
+
+//   connectToDb()
+//     .then(() => {
+//       models.note.findById(event.pathParameters.id)
+//         .then(note => callback(null, {
+//           statusCode: 200,
+//           body: JSON.stringify(note)
+//         }))
+//         .catch(err => callback(null, {
+//           statusCode: err.statusCode || 500,
+//           headers: { 'Content-Type': 'text/plain' },
+//           body: 'Could not fetch the note.'
+//         }));
+//     });
+// };
+
+// module.exports.update = (event, context, callback) => {
+//   context.callbackWaitsForEmptyEventLoop = false;
+
+//   connectToDb()
+//     .then(() => {
+//       models.note.findByIdAndUpdate(event.pathParameters.id, JSON.parse(event.body), { new: true })
+//         .then(note => callback(null, {
+//           statusCode: 200,
+//           body: JSON.stringify(note)
+//         }))
+//         .catch(err => callback(null, {
+//           statusCode: err.statusCode || 500,
+//           headers: { 'Content-Type': 'text/plain' },
+//           body: 'Could not fetch the notes.'
+//         }));
+//     });
+// };
+
+// module.exports.delete = (event, context, callback) => {
+//   context.callbackWaitsForEmptyEventLoop = false;
+
+//   connectToDb()
+//     .then(() => {
+//       models.note.findByIdAndRemove(event.pathParameters.id)
+//         .then(note => callback(null, {
+//           statusCode: 200,
+//           body: JSON.stringify({ message: 'Removed note with id: ' + note._id, note: note })
+//         }))
+//         .catch(err => callback(null, {
+//           statusCode: err.statusCode || 500,
+//           headers: { 'Content-Type': 'text/plain' },
+//           body: 'Could not fetch the notes.'
+//         }));
+//     });
+// };
