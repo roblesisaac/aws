99ed5808ceb6f8e6a0824df7587a2b48c50c2602
roblesisaac <irobles1030@gmail.com>
2018-02-21 18:51:57 -0800

    Update api.js

--- api.js
+++ api.js
@@ -6,9 +6,9 @@ const models = {
   sheets: require('./models/sheets')
 };
 
-var dbSetup = function(event, context, fn) {
+var setup = function(event, context, fn) {
   context.callbackWaitsForEmptyEventLoop = false;
-  var res = {
+  var site = {
     name: event.pathParameters.sitename,
     sheet: event.pathParameters.sheet,
     model: models[event.pathParameters.sheet] || mongoose.model(url, new mongoose.Schema({name: String},{strict: false})),
@@ -18,23 +18,33 @@ var dbSetup = function(event, context, fn) {
       body: 'Could not create the note.'      
     }
   };
-  connectToDb().then(() => fn(res));  
+  connectToDb().then(() => fn(site));  
+};
+
+module.exports.test = (event, context, callback) => {
+  callback(null, {
+    statusCode: 200,
+    body: {
+      context: context,
+      event: event
+    }
+  });
 };
 
 
 module.exports.post = (event, context, callback) => {
-  dbSetup(event, context, function(res) {
-    res.model.create(JSON.parse(event.body))
+  setup(event, context, function(site) {
+    site.model.create(JSON.parse(event.body))
       .then(data => callback(null, {
         statusCode: 200,
         body: JSON.stringify(data)
       }))
-      .catch(err => callback(null, res.err));     
+      .catch(err => callback(null, site.err));     
   });
 };
 
 module.exports.get = (event, context, callback) => {
-  dbSetup(event, context, function(site) {
+  setup(event, context, function(site) {
     site.model.find({})
       .then(data => callback(null, {
         statusCode: 200,
@@ -45,34 +55,41 @@ module.exports.get = (event, context, callback) => {
 };
 
 module.exports.getOne = (event, context, callback) => {
-  dbSetup(event, context, function(res) {
-    res.model.findById(event.pathParameters.id)
+  setup(event, context, function(site) {
+    site.model.findById(event.pathParameters.id)
       .then(data => callback(null, {
         statusCode: 200,
         body: JSON.stringify(data)
       }))
-      .catch(err => callback(null, res.err));  
+      .catch(err => callback(null, site.err));  
   });
 };
 
 module.exports.put = (event, context, callback) => {
-  dbSetup(event, context, function(res) {
-      res.model.findByIdAndUpdate(event.pathParameters.id, JSON.parse(event.body), { new: true })
+  setup(event, context, function(site) {
+      site.model.findByIdAndUpdate(event.pathParameters.id, JSON.parse(event.body), { new: true })
         .then(data => callback(null, {
           statusCode: 200,
           body: JSON.stringify(data)
         }))
-        .catch(err => callback(null, res.err));  
+        .catch(err => callback(null, site.err));  
   });
 };
 
-module.exports.delete = (event, context, callback) => {
-  dbSetup(event, context, function(res) {
-      res.model.findByIdAndRemove(event.pathParameters.id)
-        .then(data => callback(null, {
-          statusCode: 200,
-          body: JSON.stringify({ message: 'Removed item with id: ' + data._id, item: data })
-        }))
-        .catch(err => callback(null, res.err));  
-  });
-};
+// module.exports.delete = (event, context, callback) => {
+//   context.callbackWaitsForEmptyEventLoop = false;
+
+//   connectToDb()
+//     .then(() => {
+//       models.note.findByIdAndRemove(event.pathParameters.id)
+//         .then(note => callback(null, {
+//           statusCode: 200,
+//           body: JSON.stringify({ message: 'Removed note with id: ' + note._id, note: note })
+//         }))
+//         .catch(err => callback(null, {
+//           statusCode: err.statusCode || 500,
+//           headers: { 'Content-Type': 'text/plain' },
+//           body: 'Could not fetch the notes.'
+//         }));
+//     });
+// };
