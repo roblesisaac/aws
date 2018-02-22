24d13055dab26c0a06e44c27a774166dcd143d6f
roblesisaac <irobles1030@gmail.com>
2018-02-21 19:09:33 -0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,57 +1,14 @@
-const jwt = require('jsonwebtoken')
-// Set in `enviroment` of serverless.yml
-const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID
-const AUTH0_CLIENT_SECRET = process.env.AUTH0_CLIENT_SECRET
+const jwt = require('jsonwebtoken');
 
-// Policy helper function
-const generatePolicy = (principalId, effect, resource) => {
-  const authResponse = {}
-  authResponse.principalId = principalId
-  if (effect && resource) {
-    const policyDocument = {}
-    policyDocument.Version = '2012-10-17'
-    policyDocument.Statement = []
-    const statementOne = {}
-    statementOne.Action = 'execute-api:Invoke'
-    statementOne.Effect = effect
-    statementOne.Resource = resource
-    policyDocument.Statement[0] = statementOne
-    authResponse.policyDocument = policyDocument
-  }
-  return authResponse
-}
-
-// Public API
-module.exports.publicEndpoint = (event, context, callback) => {
-  return callback(null, {
+module.exports.test = (event, context, callback) => {
+  callback(null, {
     statusCode: 200,
-    headers: {
-      /* Required for CORS support to work */
-      "Access-Control-Allow-Origin": "*",
-      /* Required for cookies, authorization headers with HTTPS */
-      "Access-Control-Allow-Credentials": true
-    },
     body: JSON.stringify({
-      message: 'Hi ⊂◉‿◉つ from Public API',
-    }),
-  })
-}
-
-// Private API
-module.exports.privateEndpoint = (event, context, callback) => {
-  return callback(null, {
-    statusCode: 200,
-    headers: {
-      /* Required for CORS support to work */
-      "Access-Control-Allow-Origin": "*",
-      /* Required for cookies, authorization headers with HTTPS */
-      "Access-Control-Allow-Credentials": true
-    },
-    body: JSON.stringify({
-      message: 'Hi ⊂◉‿◉つ from Private API. Only logged in users can see this',
-    }),
-  })
-}
+      context: context,
+      event: event
+    })
+  });
+};
 
 // Reusable Authorizer function, set on `authorizer` field in serverless.yml
 module.exports.auth = (event, context, callback) => {
