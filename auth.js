03abe59f0e16af75bf78505e6e3a6ad1da0a693e
roblesisaac <irobles1030@gmail.com>
2018-02-22 16:35:00 -0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -51,55 +51,24 @@ const checkToken = (token, userId, next) => {
     });
 };
 
-module.exports.test = (event, context, callback) => {
-  context.callbackWaitsForEmptyEventLoop = false;
-  const token = event.headers['ply-token'];
-  const userid = event.headers.userid;
-  if(token && userid) {
-    checkToken(token, userid, (res) => {
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify(res)
-      });
-    });
-  } else {
-    return callback('No auth provided');
-  }
-};
-
-// Reusable Authorizer function, set on `authorizer` field in serverless.yml
 module.exports.auth = (event, context, callback) => {
-  console.log('event', event)
-  if (!event.authorizationToken) {
-    return callback('Unauthorized')
-  }
-
-  const tokenParts = event.authorizationToken.split(' ')
-  const tokenValue = tokenParts[1]
-
-  if (!(tokenParts[0].toLowerCase() === 'bearer' && tokenValue)) {
-    // no auth token!
-    return callback('Unauthorized')
-  }
-  const options = {
-    audience: AUTH0_CLIENT_ID,
-  }
-  // decode base64 secret. ref: http://bit.ly/2hA6CrO
-  const secret = new Buffer.from(AUTH0_CLIENT_SECRET, 'base64')
-  try {
-    jwt.verify(tokenValue, secret, options, (verifyError, decoded) => {
-      if (verifyError) {
-        console.log('verifyError', verifyError)
-        // 401 Unauthorized
-        console.log(`Token invalid. ${verifyError}`)
-        return callback('Unauthorized')
-      }
-      // is custom authorizer function
-      console.log('valid from customAuthorizer', decoded)
-      return callback(null, generatePolicy(decoded.sub, 'Allow', event.methodArn))
+  context.callbackWaitsForEmptyEventLoop = false;
+  // const token = event.headers['ply-token'];
+  // const userid = event.headers.userid;
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      event: event
     })
-   } catch (err) {
-    console.log('catch error. Invalid token', err)
-    return callback('Unauthorized')
-  }
-}
+  });
+  // if(token && userid) {
+  //   checkToken(token, userid, (res) => {
+  //     callback(null, {
+  //       statusCode: 200,
+  //       body: JSON.stringify(res)
+  //     });
+  //   });
+  // } else {
+  //   return callback('No auth provided');
+  // }
+};
