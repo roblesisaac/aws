476ccfeedcf356362945d6e227336d6601903a44
roblesisaac <irobles1030@gmail.com>
2018-02-22 18:53:22 -0800

    Update api.js

--- api.js
+++ api.js
@@ -25,7 +25,7 @@ var setup = function(event, context, fn) {
 const createModel = (event, context, next) => {
   context.callbackWaitsForEmptyEventLoop = false;
   const site = { url: event.pathParameters.sitename, sheet: event.pathParameters.sheet };
-  next(null, {site: 'her it is'});
+  next({erro: 'sjkdlfkldsajfklds'}, {site: 'her it is'});
   // connectToDb().then(() => {
   //   //get site
   //   models.sites.findOne({ url: site.url })
