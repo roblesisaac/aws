a23286b6dad7654400bc9ed655a1c13c351c241d
roblesisaac <irobles1030@gmail.com>
2018-03-01 15:03:22 -0800

    Update api.js

--- api.js
+++ api.js
@@ -50,25 +50,22 @@ const createModel = (event, context, next) => {
   });  
 };
 
-const printError = (callback, error) => {
-  callback({
+const printError = (error) => {
+  return {
     statusCode: 200,
     body: JSON.stringify({ error: error })    
-  });
+  };
 };
 
 module.exports.test = (event, context, callback) => {
   createModel(event, context, function(error, model) {
-    if(error) {
-      printError(callback, error);
-    } else {
-      model.find({})
-        .then(data => callback(null, {
-          statusCode: 200,
-          body: JSON.stringify(data)
-        }))
-        .catch(err => callback(null, err));  
-    }
+    if(error) return callback(printError(error));
+    model.find({})
+      .then(data => callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(data)
+      }))
+      .catch(err => callback(null, err)); 
   });
 };
 
