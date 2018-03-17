0e8dfa02470aca3aeae0b7aea8d8fee7db5eca40
roblesisaac <irobles1030@gmail.com>
2018-03-16 17:07:55 -0700

    Update api.js

--- api.js
+++ api.js
@@ -147,20 +147,14 @@ module.exports.put = (event, context, callback) => {
   });
 };
 
-// module.exports.delete = (event, context, callback) => {
-//   context.callbackWaitsForEmptyEventLoop = false;
-
-//   connectToDb()
-//     .then(() => {
-//       models.note.findByIdAndRemove(event.pathParameters.id)
-//         .then(note => callback(null, {
-//           statusCode: 200,
-//           body: JSON.stringify({ message: 'Removed note with id: ' + note._id, note: note })
-//         }))
-//         .catch(err => callback(null, {
-//           statusCode: err.statusCode || 500,
-//           headers: { 'Content-Type': 'text/plain' },
-//           body: 'Could not fetch the notes.'
-//         }));
-//     });
-// };
+module.exports.delete = (event, context, callback) => {
+  getModel(event, context, function(error, model) {
+    if(error) return printError(callback, error);
+      model.findByIdAndRemove(event.pathParameters.id)
+        .then(data => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(data)
+        }))
+      .catch(err => callback(null, err)); 
+  });
+};
