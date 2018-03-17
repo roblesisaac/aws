46cad8b1422c46abb1c262ab313ab0317aa7f403
roblesisaac <irobles1030@gmail.com>
2018-03-16 17:22:47 -0700

    Update api.js

--- api.js
+++ api.js
@@ -93,7 +93,7 @@ module.exports.component = (event, context, callback) => {
       headers: {
         'Content-Type': 'application/javascript',
       },
-      body: sheet.script || 'no ' + event.pathParameters.prop
+      body: sheet || 'no ' + event.pathParameters.prop
     });
   });
 };
