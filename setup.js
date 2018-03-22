226491152eaebc0f8169b40bced1a5051bfcd5f2
roblesisaac <irobles1030@gmail.com>
2018-03-21 20:22:58 -0700

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,6 +1,4 @@
 const connectToDb = require('./db');
-const mongoose = require('mongoose');
-const checkToken = require('./auth').checkToken;
 const models = {
   users: require('./models/users'),
   sites: require('./models/sites'),
@@ -25,24 +23,31 @@ function createFirst(name, data, next) {
 }
 
 module.exports.init = (event, context, callback) => {
-  context.callbackWaitsForEmptyEventLoop = false;
-  connectToDb().then(() => {
-    const firstUser = { name: 'Eiken', password: 'pass' };
-    areThereAnyYet('users', firstUser, function(user) {
-      const firstSite = { name: 'plysheet', userId: user._id, url: 'plysheet' };
-      areThereAnyYet('sites', firstSite, function(site) {
-        if(sites.length === 0) {
-          createFirst('sites', function(site) {
-            callback(null, {
-              statusCode: 200,
-              body: JSON.stringify({
-                user:user,
-                site: site
-              })
-            });            
-          });
-        }
-      });  
-    });
-  });
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      user:'user',
+      site: 'site'
+    })
+  });  
+  // context.callbackWaitsForEmptyEventLoop = false;
+  // connectToDb().then(() => {
+  //   const firstUser = { name: 'Eiken', password: 'pass' };
+  //   areThereAnyYet('users', firstUser, function(user) {
+  //     const firstSite = { name: 'plysheet', userId: user._id, url: 'plysheet' };
+  //     areThereAnyYet('sites', firstSite, function(site) {
+  //       if(sites.length === 0) {
+  //         createFirst('sites', function(site) {
+  //           callback(null, {
+  //             statusCode: 200,
+  //             body: JSON.stringify({
+  //               user:user,
+  //               site: site
+  //             })
+  //           });            
+  //         });
+  //       }
+  //     });  
+  //   });
+  // });
 };
