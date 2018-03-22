28f64b873e11d4857dda3a5e80e2665d6cdadf11
roblesisaac <irobles1030@gmail.com>
2018-03-21 20:19:25 -0700

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,50 +1,48 @@
-// const connectToDb = require('./db');
-// const models = {
-//   users: require('./models/users'),
-//   sites: require('./models/sites'),
-//   sheets: require('./models/sheets')
-// };
+const connectToDb = require('./db');
+const mongoose = require('mongoose');
+const checkToken = require('./auth').checkToken;
+const models = {
+  users: require('./models/users'),
+  sites: require('./models/sites'),
+  sheets: require('./models/sheets')
+};
 
-// function areThereAnyYet(name, data, next) {
-//   models[name].find().then(function(res) {
-//     if(res.length === 0) {
-//       createFirst(name, data, function(firstItem) {
-//         next(firstItem);
-//       });
-//     } else {
-//       next(res[0]);
-//     }
-//   });      
-// }
-// function createFirst(name, data, next) {
-//   models[name].create(data).then(function(res){
-//     next(res);
-//   });
-// }
+function areThereAnyYet(name, data, next) {
+  models[name].find().then(function(res) {
+    if(res.length === 0) {
+      createFirst(name, data, function(firstItem) {
+        next(firstItem);
+      });
+    } else {
+      next(res[0]);
+    }
+  });      
+}
+function createFirst(name, data, next) {
+  models[name].create(data).then(function(res){
+    next(res);
+  });
+}
 
-module.exports.setUp = (event, context, callback) => {
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify(['hi'])
-  }); 
-  // context.callbackWaitsForEmptyEventLoop = false;
-  // connectToDb().then(() => {
-  //   const firstUser = { name: 'Eiken', password: 'pass' };
-  //   areThereAnyYet('users', firstUser, function(user) {
-  //     const firstSite = { name: 'plysheet', userId: user._id, url: 'plysheet' };
-  //     areThereAnyYet('sites', firstSite, function(site) {
-  //       if(sites.length === 0) {
-  //         createFirst('sites', function(site) {
-  //           callback(null, {
-  //             statusCode: 200,
-  //             body: JSON.stringify({
-  //               user:user,
-  //               site: site
-  //             })
-  //           });            
-  //         });
-  //       }
-  //     });  
-  //   });
-  // });
+module.exports.init = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  connectToDb().then(() => {
+    const firstUser = { name: 'Eiken', password: 'pass' };
+    areThereAnyYet('users', firstUser, function(user) {
+      const firstSite = { name: 'plysheet', userId: user._id, url: 'plysheet' };
+      areThereAnyYet('sites', firstSite, function(site) {
+        if(sites.length === 0) {
+          createFirst('sites', function(site) {
+            callback(null, {
+              statusCode: 200,
+              body: JSON.stringify({
+                user:user,
+                site: site
+              })
+            });            
+          });
+        }
+      });  
+    });
+  });
 };
