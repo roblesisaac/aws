4f0edc45f3b0e6e0bb346f9e117623b1d94bbc56
roblesisaac <irobles1030@gmail.com>
2018-03-21 20:08:07 -0700

    Update api.js

--- api.js
+++ api.js
@@ -85,6 +85,46 @@ const printError = (callback, error) => {
   });
 };
 
+function areThereAnyYet(name, data, next) {
+  models[name].find().then(function(res) {
+    if(res.length === 0) {
+      createFirst(name, data, function(firstItem) {
+        next(firstItem);
+      });
+    } else {
+      next(res[0]);
+    }
+  });      
+}
+function createFirst(name, data, next) {
+  models[name].create(data).then(function(res){
+    next(res);
+  });
+}
+
+module.exports.setUp = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  connectToDb().then(() => {
+    const firstUser = { name: 'Eiken', password: 'pass' };
+    areThereAnyYet('users', firstUser, function(user) {
+      const firstSite = { name: 'plysheet', userId: user._id, url: 'plysheet' };
+      areThereAnyYet('sites', firstSite, function(site) {
+        if(sites.length === 0) {
+          createFirst('sites', function(site) {
+            callback(null, {
+              statusCode: 200,
+              body: JSON.stringify({
+                user:user,
+                site: site
+              })
+            });            
+          });
+        }
+      });  
+    });
+  });
+};
+
 module.exports.sheetProp = (event, context, callback) => {
   findSheet(event, context, function(err, sheet){
     if(err) return printError(callback, err);
