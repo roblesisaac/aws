69ca60fccef8df90f3d4a6290d70c598f87292fa
roblesisaac <irobles1030@gmail.com>
2018-03-21 20:26:51 -0700

    Update setup.js

--- setup.js
+++ setup.js
@@ -26,21 +26,27 @@ module.exports.init = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
   connectToDb().then(() => {
     const firstUser = { name: 'Eiken', password: 'pass' };
-    areThereAnyYet('users', firstUser, function(user) {
-      const firstSite = { name: 'plysheet', userId: user._id, url: 'plysheet' };
-      areThereAnyYet('sites', firstSite, function(site) {
-        if(sites.length === 0) {
-          createFirst('sites', function(site) {
-            callback(null, {
-              statusCode: 200,
-              body: JSON.stringify({
-                user: user,
-                site: site
-              })
-            });            
-          });
-        }
-      });  
-    });
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          user: 'user'
+        })
+      });
+    // areThereAnyYet('users', firstUser, function(user) {
+    //   const firstSite = { name: 'plysheet', userId: user._id, url: 'plysheet' };
+    //   areThereAnyYet('sites', firstSite, function(site) {
+    //     if(sites.length === 0) {
+    //       createFirst('sites', function(site) {
+    //         callback(null, {
+    //           statusCode: 200,
+    //           body: JSON.stringify({
+    //             user: user,
+    //             site: site
+    //           })
+    //         });            
+    //       });
+    //     }
+    //   });  
+    // });
   });
 };
