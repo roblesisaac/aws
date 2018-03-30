91746f547ad0cd1b4e62abcf22d562561f92b7de
roblesisaac <irobles1030@gmail.com>
2018-03-30 13:56:53 -0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,35 +1,100 @@
+const checkToken = require('./auth').checkToken;
+const DB = process.env.DB;
 const jwt = require('jsonwebtoken');
-const users = require('./models/users');
-const connectToDb = require('./db');
-const ply = require('ply');
-
-module.exports.login = (event, context, callback) => {
-  const user = JSON.parse(event.body);
-  ply.login(context, user, function(err, tokenObj){
-    if(err) {
-      ply.error(callback, err);     
-    } else {
-      ply.res(callback, JSON.stringify(tokenObj));
-    }
-  });
+const models = {
+  sites: require('./models/sites'),
+  sheets: require('./models/sheets'),
+  users: require('./models/users')
 };
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+let isConnected;
 
-module.exports.checkToken1 = (event, context, next) => {
-  context.callbackWaitsForEmptyEventLoop = false;
-  const token = event.headers['ply-token'];
-  const userId = event.headers.userid;
-  if(!token || !userId) return next({success: false, message: 'No token or userid provided'});
-  connectToDb()
-    .then(() => {
-    	users.findById(userId, (err, user) => {
-    		if(!user) return next({success: false, message: 'nope'});
-        jwt.verify(token, user.password, (err, decoded) => {
-    			if (err) {
-    				next({ success: false, message: 'You are logged out.' });
+const ply = {
+  connect: function(context) {
+    if(context) context.callbackWaitsForEmptyEventLoop = false;
+    if (isConnected) {
+      return Promise.resolve();
+    }
+    return mongoose.connect(DB)
+      .then(db => { 
+        isConnected = db.connections[0].readyState;
+      });    
+  },
+  checkToken: function(context, token, userId, next) {
+    if(!token || !userId) return next('No token or userid provided');
+    this.connect(context).then(function(){
+    	models.users.findById(userId, (err, user) => {
+    		if(!user) return next('no user found with this id: '+userId);
+        jwt.verify(token, user.password, (err2, decoded) => {
+    			if (err2) {
+    				next('You are logged out with this error: '+ err2);
     			} else {
-    				next({success: true});
+    				next(null, decoded);
     			}
     		});
-    	});      
+    	}); 
     });
+  },
+  error: function(callback, err) {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({
+        error: err
+      })
+    }); 
+  },
+  findSheet: function(event, context, next) {
+    const path = { url: event.pathParameters.sitename, sheet: event.pathParameters.sheet };
+    this.connect(context).then(() => {
+      // get site
+      models.sites.findOne({ url: path.url })
+        .then(site => {
+          if(!site) return next(path.url + ' plysheet not found.');
+          //get sheet
+          models.sheets.findOne({ siteId: site._id, name: path.sheet })
+            .then(sheet => {
+              if(!sheet) return next(path.url + ' plysheet found but no ' + path.sheet + ' sheet found.');
+              next(null, sheet);
+            });
+        });
+    });      
+  },
+  login: function(context, user, next) {
+    this.connect(context).then(function(){
+    	models.users.findOne({username: user.username}, function(err, foundUser) {
+    		if (err) {
+    		  next(err);
+    		  return;
+    		}
+    		if (!foundUser) {
+    			next(user.username + ' not found');
+    		} else if (foundUser) {
+    			foundUser.comparePassword(user.password, function(err2, isMatch){
+    				if(isMatch && isMatch === true) {
+    					next(null, {
+    					  'plyToken': jwt.sign({ _id: foundUser._id, username: foundUser.username, name: foundUser.name,	password: foundUser.password	}, foundUser.password, {	expiresIn: '15h' }),
+    					  userId: foundUser._id
+    					});
+    				} else {
+    					next('Authentication failed. Wrong password.');
+    				}
+    			});
+    		}
+    	});
+    });
+  },
+  res: function(callback, body) {
+    callback(null, {
+      statusCode: 200,
+      body: body
+    }); 
+  },
+  sheets: function() {
+    return 'test four';
+  }
 };
+
+for (var key in ply) {
+  if(key.indexOf('_') === -1) module.exports[key] = ply[key];
+}
