8cb4ac24b415e144824719f4e4d77a6067b4770f
roblesisaac <irobles1030@gmail.com>
2018-03-30 12:43:23 -0700

    Update ply.js

--- ply.js
+++ ply.js
@@ -1,55 +1,45 @@
-const mongoose = require('mongoose');
 const checkToken = require('./auth').checkToken;
 const DB = process.env.DB;
-mongoose.Promise = global.Promise;
-let isConnected;
 const models = {
   sites: require('./models/sites'),
   sheets: require('./models/sheets')
 };
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+let isConnected;
 
-module.exports.connect = (context) => {
-  if(context) context.callbackWaitsForEmptyEventLoop = false;
-  if (isConnected) {
-    return Promise.resolve();
+const ply = {
+  connect: function(context) {
+    if(context) context.callbackWaitsForEmptyEventLoop = false;
+    if (isConnected) {
+      return Promise.resolve();
+    }
+    return mongoose.connect(DB)
+      .then(db => { 
+        isConnected = db.connections[0].readyState;
+      });    
+  },
+  findSheet: function(event, context, next) {
+    const path = { url: event.pathParameters.sitename, sheet: event.pathParameters.sheet };
+    this.connect(context).then(() => {
+      // get site
+      models.sites.findOne({ url: path.url })
+        .then(site => {
+          if(!site) return next(path.url + ' plysheet not found.');
+          //get sheet
+          models.sheets.findOne({ siteId: site._id, name: path.sheet })
+            .then(sheet => {
+              if(!sheet) return next(path.url + ' plysheet found but no ' + path.sheet + ' sheet found.');
+              next(null, sheet);
+            });
+        });
+    });      
+  },
+  sheets: function() {
+    return 'test three';
   }
-  return mongoose.connect(DB)
-    .then(db => { 
-      isConnected = db.connections[0].readyState;
-    });
 };
 
-// module.exports.find = (modelName, query, next) => {
-//   module.exports.connect(context, function() {
-//     models[modelName].findOne(query).then(function(data){
-//       next(data);
-//     });
-//   });
-// };
-
-// module.exports.findSheet = () => {
-//   ply.find('sites', { url: path.url }, function() {
-    
-//   });
-// };
-
-module.exports.findSheet = (event, context, next) => {
-  const path = { url: event.pathParameters.sitename, sheet: event.pathParameters.sheet };
-  module.exports.connect(context).then(() => {
-    // get site
-    models.sites.findOne({ url: path.url })
-      .then(site => {
-        if(!site) return next(path.url + ' plysheet not found.');
-        //get sheet
-        models.sheets.findOne({ siteId: site._id, name: path.sheet })
-          .then(sheet => {
-            if(!sheet) return next(path.url + ' plysheet found but no ' + path.sheet + ' sheet found.');
-            next(null, sheet);
-          });
-      });
-  });   
-};
-
-module.exports.sheets = function() {
-  return 'test two';
-};
+for (var key in ply) {
+  if(key.indexOf('_') === -1) module.exports[key] = ply[key];
+}
