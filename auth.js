c6ecee44e1fce2e5bc9431b9f237390f0badd421
roblesisaac <irobles1030@gmail.com>
2018-03-30 14:23:47 -0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,3 @@
-const jwt = require('jsonwebtoken');
-const users = require('./models/users');
-const connectToDb = require('./db');
 const ply = require('ply');
 
 module.exports.login = (event, context, callback) => {
@@ -12,24 +9,4 @@ module.exports.login = (event, context, callback) => {
       ply.res(callback, JSON.stringify(tokenObj));
     }
   });
-};
-
-module.exports.checkToken1 = (event, context, next) => {
-  context.callbackWaitsForEmptyEventLoop = false;
-  const token = event.headers['ply-token'];
-  const userId = event.headers.userid;
-  if(!token || !userId) return next({success: false, message: 'No token or userid provided'});
-  connectToDb()
-    .then(() => {
-    	users.findById(userId, (err, user) => {
-    		if(!user) return next({success: false, message: 'nope'});
-        jwt.verify(token, user.password, (err, decoded) => {
-    			if (err) {
-    				next({ success: false, message: 'You are logged out.' });
-    			} else {
-    				next({success: true});
-    			}
-    		});
-    	});      
-    });
 };
