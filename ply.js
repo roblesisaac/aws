13064a1aeb96bbb460a8f1dfe70df4742e9c64d6
roblesisaac <irobles1030@gmail.com>
2018-04-11 16:33:18 -0700

    Update ply.js

--- ply.js
+++ ply.js
@@ -295,13 +295,13 @@ module.exports.port = function(event, context, callback) {
   const params = event.pathParameters || {};
   const fn = ply[params.method] || ply.landing;
   ply.connect(context).then(function(){
-    if(params.site) {
-      fn(event, context, function(err, body, contentType) {
-        if(err) return res.error(callback, err);
-        res.body(callback, body, contentType);
-      });
-    } else {
-      res.body(callback, 'no site to see');  
+    if(!params.site) {
+      params.site = 'plysheet';
+      fn = ply.landing;
     }
+    fn(event, context, function(err, body, contentType) {
+      if(err) return res.error(callback, err);
+      res.body(callback, body, contentType);
+    });
   });
 }
