75a9c87aff26854b1bdbe95d6ed5cb3b9b753af1
roblesisaac <irobles1030@gmail.com>
2018-05-25 15:04:33 -0700

    Update ply.js

--- ply.js
+++ ply.js
@@ -42,7 +42,7 @@ const res = {
 };
 
 const ply = {
-  api: function(event, context, send) {
+  api: function(event, context, send, callback) {
     ply.getModel(siteName, sheetName, o.event, function(err, model, sheet, site) {
       if(err) return send(err);
       model.find({}).then(function(data){
