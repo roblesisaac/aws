2c28dfe40d3cc801167fd31439741a2b31dff53c
roblesisaac <irobles1030@gmail.com>
2018-06-15 17:16:51 -0700

    Update ply.js

--- ply.js
+++ ply.js
@@ -59,7 +59,7 @@ const ply = {
               const filters = {};
               for(var key in parameters) {
                 if(keys.indexOf(key) > -1) {
-                  filters[key] = 2;
+                  filters[key] = parameters[key];
                   delete parameters[key];
                 }
               }
